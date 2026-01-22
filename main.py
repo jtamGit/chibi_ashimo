72e3cb5e5014f45549fb2a56a146826d1ef277a7
jtamGit <86784608+jtamGit@users.noreply.github.com>
2026-01-22 16:44:22 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,25 +3,60 @@ def on_received_string(receivedString):
     led.toggle(0, 0)
 radio.on_received_string(on_received_string)
 
+def on_received_value(name, value):
+    global x_bar, y_bar, button
+    if name == "x":
+        x_bar = value
+        led.toggle(1, 0)
+    if name == "y":
+        y_bar = value
+        led.toggle(2, 0)
+    if name == "button":
+        button = value
+        led.toggle(3, 0)
+radio.on_received_value(on_received_value)
+
+y_speed = 0
 res = ""
+button = 0
+y_bar = 0
+x_bar = 0
 radio.set_group(1)
-serial.redirect(SerialPin.P8, SerialPin.P12, BaudRate.BAUD_RATE9600)
+radio.set_transmit_power(7)
 serial.set_tx_buffer_size(1000)
 serial.set_rx_buffer_size(1000)
+serial.redirect(SerialPin.P8, SerialPin.P12, BaudRate.BAUD_RATE4800)
 images.create_image("""
     . . . . .
-    . . . . .
-    . . . . .
+    . # # # .
+    # . . . #
     # . . . #
     . # # # .
     """).show_image(0)
+servos.P0.set_pulse(1500)
+servos.P1.set_pulse(1500)
 
 def on_forever():
-    global res
+    global res, y_speed
     res = serial.read_string()
     if res.is_empty():
         pass
     else:
         radio.send_string(res)
         led.toggle(4, 0)
+    y_speed = y_bar - 499
+    servos.P0.set_pulse(1500 - y_speed + (x_bar - 480))
+    servos.P1.set_pulse(1500 + y_speed + (x_bar - 480))
+    if button < 258:
+        serial.write_line("konnnichiwa")
+    elif button < 600:
+        serial.write_line("yukkurishiteittene")
+    elif button < 725:
+        serial.write_line("soretteanatanokannsoudesuyone?")
+    elif button < 809:
+        serial.write_line("bunnbunnharo-yu-chu-bu")
+    elif button < 834:
+        serial.write_line("gomennnasai yokuwakarimasenn")
+    elif button < 918:
+        serial.write_line("?")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,15 +2,34 @@ radio.onReceivedString(function (receivedString) {
     serial.writeString(receivedString)
     led.toggle(0, 0)
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        x_bar = value
+        led.toggle(1, 0)
+    }
+    if (name == "y") {
+        y_bar = value
+        led.toggle(2, 0)
+    }
+    if (name == "button") {
+        button = value
+        led.toggle(3, 0)
+    }
+})
+let y_speed = 0
 let res = ""
+let button = 0
+let y_bar = 0
+let x_bar = 0
 radio.setGroup(1)
+radio.setTransmitPower(7)
+serial.setTxBufferSize(1000)
+serial.setRxBufferSize(1000)
 serial.redirect(
 SerialPin.P8,
 SerialPin.P12,
 BaudRate.BaudRate4800
 )
-serial.setTxBufferSize(1000)
-serial.setRxBufferSize(1000)
 images.createImage(`
     . . . . .
     . # # # .
@@ -18,6 +37,8 @@ images.createImage(`
     # . . . #
     . # # # .
     `).showImage(0)
+servos.P0.setPulse(1500)
+servos.P1.setPulse(1500)
 basic.forever(function () {
     res = serial.readString()
     if (res.isEmpty()) {
@@ -25,5 +46,27 @@ basic.forever(function () {
     } else {
         radio.sendString(res)
         led.toggle(4, 0)
+    }
+    y_speed = y_bar - 499
+    servos.P0.setPulse(1500 - y_speed + (x_bar - 480))
+    servos.P1.setPulse(1500 + y_speed + (x_bar - 480))
+    if (button < 258) {
+        serial.writeLine("konnnichiwa")
+        basic.pause(200)
+    } else if (button < 600) {
+        serial.writeLine("yukkurishiteittene")
+        basic.pause(200)
+    } else if (button < 725) {
+        serial.writeLine("soretteanatanokannsoudesuyone?")
+        basic.pause(200)
+    } else if (button < 809) {
+        serial.writeLine("bunnbunnharo-yu-chu-bu")
+        basic.pause(200)
+    } else if (button < 834) {
+        serial.writeLine("gomennnasai yokuwakarimasenn")
+        basic.pause(200)
+    } else if (button < 918) {
+        serial.writeLine("?")
+        basic.pause(200)
     }
 })
