069b0439c106e47fd24acb767de0f1282da4b2b3
jtamGit <86784608+jtamGit@users.noreply.github.com>
2026-01-23 16:58:53 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ def on_received_string(receivedString):
     led.toggle(0, 0)
 radio.on_received_string(on_received_string)
 
+def sound_template():
+    if button < 258:
+        serial.write_line("konnnichiwa")
+        basic.pause(200)
+    elif button < 600:
+        serial.write_line("yukkurishiteittene")
+        basic.pause(200)
+    elif button < 725:
+        serial.write_line("soretteanatanokannsoudesuyone?")
+        basic.pause(200)
+    elif button < 809:
+        serial.write_line("bunnbunnharo-yu-chu-bu")
+        basic.pause(200)
+    elif button < 834:
+        serial.write_line("gomennnasai yokuwakarimasenn")
+        basic.pause(200)
+    elif button < 918:
+        serial.write_line("?")
+        basic.pause(200)
+
 def on_received_value(name, value):
     global x_bar, y_bar, button
     if name == "x":
@@ -17,10 +37,19 @@ def on_received_value(name, value):
 radio.on_received_value(on_received_value)
 
 y_speed = 0
+sensor_old = 0
 res = ""
-button = 0
 y_bar = 0
 x_bar = 0
+button = 0
+mode = 0
+spd_stop = 1500
+R_spd_H = 1500 - 50
+R_spd_L = 1500 - 0
+L_spd_H = 1500 + 50
+L_spd_L = 1500 + 0
+servos.P0.stop()
+servos.P1.stop()
 radio.set_group(1)
 radio.set_transmit_power(7)
 serial.set_tx_buffer_size(1000)
@@ -33,30 +62,48 @@ images.create_image("""
     # . . . #
     . # # # .
     """).show_image(0)
-servos.P0.set_pulse(1500)
-servos.P1.set_pulse(1500)
+if input.button_is_pressed(Button.A):
+    mode = 1
+else:
+    mode = 0
 
 def on_forever():
-    global res, y_speed
+    global res, sensor_old, y_speed
     res = serial.read_string()
     if res.is_empty():
         pass
     else:
         radio.send_string(res)
         led.toggle(4, 0)
-    y_speed = y_bar - 499
-    servos.P0.set_pulse(1500 - y_speed + (x_bar - 480))
-    servos.P1.set_pulse(1500 + y_speed + (x_bar - 480))
-    if button < 258:
-        serial.write_line("konnnichiwa")
-    elif button < 600:
-        serial.write_line("yukkurishiteittene")
-    elif button < 725:
-        serial.write_line("soretteanatanokannsoudesuyone?")
-    elif button < 809:
-        serial.write_line("bunnbunnharo-yu-chu-bu")
-    elif button < 834:
-        serial.write_line("gomennnasai yokuwakarimasenn")
-    elif button < 918:
-        serial.write_line("?")
+    if mode == 0:
+        sencer = pins.analog_read_pin(AnalogReadWritePin.P2)
+        if sencer < 134:
+            # 左:ON  右:ON
+            servos.P0.set_pulse(R_spd_H)
+            servos.P1.set_pulse(L_spd_H)
+            sensor_old = sencer
+        elif sencer < 390:
+            # 左:OFF  右:ON
+            servos.P0.set_pulse(R_spd_L)
+            servos.P1.set_pulse(L_spd_H)
+            sensor_old = sencer
+        elif sencer < 646:
+            # 左:ON  右:OFF
+            servos.P0.set_pulse(R_spd_H)
+            servos.P1.set_pulse(L_spd_L)
+            sensor_old = sencer
+        elif sensor_old < 390:
+            servos.P0.stop()
+            servos.P1.set_pulse(L_spd_L)
+        elif sensor_old < 646:
+            servos.P0.set_pulse(R_spd_L)
+            servos.P1.stop()
+        else:
+            servos.P0.stop()
+            servos.P1.stop()
+    else:
+        sound_template()
+        y_speed = y_bar - 499
+        servos.P0.set_pulse(1500 - y_speed + (x_bar - 480) / 2)
+        servos.P1.set_pulse(1500 + y_speed + (x_bar - 480) / 2)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,54 +2,7 @@ radio.onReceivedString(function (receivedString) {
     serial.writeString(receivedString)
     led.toggle(0, 0)
 })
-radio.onReceivedValue(function (name, value) {
-    if (name == "x") {
-        x_bar = value
-        led.toggle(1, 0)
-    }
-    if (name == "y") {
-        y_bar = value
-        led.toggle(2, 0)
-    }
-    if (name == "button") {
-        button = value
-        led.toggle(3, 0)
-    }
-})
-let y_speed = 0
-let res = ""
-let button = 0
-let y_bar = 0
-let x_bar = 0
-radio.setGroup(1)
-radio.setTransmitPower(7)
-serial.setTxBufferSize(1000)
-serial.setRxBufferSize(1000)
-serial.redirect(
-SerialPin.P8,
-SerialPin.P12,
-BaudRate.BaudRate4800
-)
-images.createImage(`
-    . . . . .
-    . # # # .
-    # . . . #
-    # . . . #
-    . # # # .
-    `).showImage(0)
-servos.P0.setPulse(1500)
-servos.P1.setPulse(1500)
-basic.forever(function () {
-    res = serial.readString()
-    if (res.isEmpty()) {
-    	
-    } else {
-        radio.sendString(res)
-        led.toggle(4, 0)
-    }
-    y_speed = y_bar - 499
-    servos.P0.setPulse(1500 - y_speed + (x_bar - 480))
-    servos.P1.setPulse(1500 + y_speed + (x_bar - 480))
+function sound_template () {
     if (button < 258) {
         serial.writeLine("konnnichiwa")
         basic.pause(200)
@@ -68,5 +21,97 @@ basic.forever(function () {
     } else if (button < 918) {
         serial.writeLine("?")
         basic.pause(200)
+    }
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        x_bar = value
+        led.toggle(1, 0)
+    }
+    if (name == "y") {
+        y_bar = value
+        led.toggle(2, 0)
+    }
+    if (name == "button") {
+        button = value
+        led.toggle(3, 0)
+    }
+})
+let y_speed = 0
+let sensor_old = 0
+let res = ""
+let y_bar = 0
+let x_bar = 0
+let button = 0
+let mode = 0
+let spd_stop = 1500
+let R_spd_H = 1500 - 50
+let R_spd_L = 1500 - 0
+let L_spd_H = 1500 + 50
+let L_spd_L = 1500 + 0
+servos.P0.stop()
+servos.P1.stop()
+radio.setGroup(1)
+radio.setTransmitPower(7)
+serial.setTxBufferSize(1000)
+serial.setRxBufferSize(1000)
+serial.redirect(
+SerialPin.P8,
+SerialPin.P12,
+BaudRate.BaudRate4800
+)
+images.createImage(`
+    . . . . .
+    . # # # .
+    # . . . #
+    # . . . #
+    . # # # .
+    `).showImage(0)
+if (input.buttonIsPressed(Button.A)) {
+    mode = 1
+} else {
+    mode = 0
+}
+basic.forever(function () {
+    let sencer: number;
+res = serial.readString()
+    if (res.isEmpty()) {
+    	
+    } else {
+        radio.sendString(res)
+        led.toggle(4, 0)
+    }
+    if (mode == 0) {
+        sencer = pins.analogReadPin(AnalogReadWritePin.P2)
+        if (sencer < 134) {
+            // 左:ON  右:ON
+            servos.P0.setPulse(R_spd_H)
+            servos.P1.setPulse(L_spd_H)
+            sensor_old = sencer
+        } else if (sencer < 390) {
+            // 左:OFF  右:ON
+            servos.P0.setPulse(R_spd_L)
+            servos.P1.setPulse(L_spd_H)
+            sensor_old = sencer
+        } else if (sencer < 646) {
+            // 左:ON  右:OFF
+            servos.P0.setPulse(R_spd_H)
+            servos.P1.setPulse(L_spd_L)
+            sensor_old = sencer
+        } else if (sensor_old < 390) {
+            servos.P0.stop()
+            servos.P1.setPulse(L_spd_L)
+        } else if (sensor_old < 646) {
+            servos.P0.setPulse(R_spd_L)
+            servos.P1.stop()
+        } else {
+            servos.P0.stop()
+            servos.P1.stop()
+        }
+    } else {
+        sound_template()
+        y_speed = y_bar - 499
+        servos.P0.setPulse(1500 - y_speed + (x_bar - 480) / 2)
+        servos.P1.setPulse(1500 + y_speed + (x_bar - 480) / 2)
     }
 })
